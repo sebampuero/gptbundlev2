3b6721885eb845b8c36ac19bbb93845a8363549c
Sebastian <sebastian@sebampuerom.de>
2025-12-20 01:43:11 +0100

    maybe we can stick to openrouter only

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,8 +2,6 @@
 
 export PYTHONPATH=$(pwd)
 
-export OPENAI_API_KEY="$(cat /run/secrets/openai_api_key)"
-export DEEPSEEK_API_KEY="$(cat /run/secrets/deepseek_api_key)"
 export OPENROUTER_API_KEY="$(cat /run/secrets/openrouter_api_key)"
 
 uv run alembic upgrade head
