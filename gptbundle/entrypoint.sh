4f6f02b5c2e1eaec3c8ee32eced38d23028d517b
Sebastian <sebatorin@hotmail.com>
2026-04-12 11:36:04 +0200

    feat: add MISTRAL_API_KEY support via docker secrets

--- gptbundle/entrypoint.sh
+++ gptbundle/entrypoint.sh
@@ -6,6 +6,7 @@ export OPENROUTER_API_KEY="$(cat /run/secrets/openrouter_api_key)"
 export JWT_SECRET_KEY="$(cat /run/secrets/jwt_secret_key)"
 export S3_ACCESS_KEY_ID="$(cat /run/secrets/s3_access_key_id)"
 export S3_SECRET_ACCESS_KEY="$(cat /run/secrets/s3_secret_access_key)"
+export MISTRAL_API_KEY="$(cat /run/secrets/mistral_api_key)"
 
 uv run alembic upgrade head
 python3 gptbundle/initial_table_bootstrap.py
